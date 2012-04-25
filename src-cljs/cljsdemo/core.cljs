74c1b1e2f052b372067aeb4b3d0dbc825632ac96
Scott Parker <scott.p.parker@gmail.com>
2012-04-25 10:18:52 -0500

    Added a basic chat handler. You can make something cooler though!

--- src-cljs/cljsdemo/core.cljs
+++ src-cljs/cljsdemo/core.cljs
@@ -3,4 +3,15 @@
   (:require [cljsdemo.utils :as utils]
             [cljsdemo.sockets :as sockets]))
 
-(utils/on-user-input (fn [msg] (.log js/console msg)))
+
+; {:identifer :chat :payload "Hello"}
+(utils/on-user-input
+  (fn [msg]
+    (sockets/send :chat msg)))
+
+; {:identifer :chat :payload "Hello" :sender {:name "Scott" :email "sparker@example.com"}
+(sockets/on-message-received
+  (fn [data]
+    (utils/append-msg-to
+      (str (get-in data [:sender :name]) ":" (:payload data))
+      :#chatlog)))
